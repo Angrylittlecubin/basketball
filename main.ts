4662597a2a29e0fcd872d8c19e7fa1c6185e5032
Angrylittlecubin <124392087+Angrylittlecubin@users.noreply.github.com>
2023-02-03 13:41:45 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,58 @@
+input.onPinPressed(TouchPin.P2, function () {
+	
+})
+let score = [
+images.createImage(`
+    . # # . .
+    # . . # .
+    # . . # .
+    # . . # .
+    . # # . .
+    `),
+images.createImage(`
+    . . # . .
+    . . # . .
+    . . # . .
+    . . # . .
+    . . # . .
+    `),
+images.createImage(`
+    . # . # .
+    . # . # .
+    . # . # .
+    . # . # .
+    . # . # .
+    `),
+0,
+images.createImage(`
+    # . # . #
+    # . # . #
+    # . # . #
+    # . # . #
+    # . # . #
+    `),
+0,
+0,
+0,
+images.createImage(`
+    . . . . .
+    # . # . #
+    # . # . #
+    # . # . #
+    # . . # .
+    `),
+images.createImage(`
+    . . . . .
+    . # . # .
+    . # . # .
+    . # . # .
+    . . # . .
+    `),
+images.createImage(`
+    . . . . .
+    # . # . #
+    # . # . #
+    # . # . #
+    . # . . #
+    `)
+]
